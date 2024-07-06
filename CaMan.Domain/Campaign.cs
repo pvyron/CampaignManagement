051fe76cba0f762644bb1e2a364cb533d2caa5b7
pvyron <pvyron@gmail.com>
2024-07-06 21:45:28 +0300

    create Campaign and Person domain models

---
+++ CaMan.Domain/Campaign.cs
@@ -0,0 +1,50 @@
+﻿namespace CaMan.Domain;
+
+public sealed class Campaign
+{
+    private Campaign(string title, string? description, Person manager)
+    {
+        Title = title;
+        Description = description;
+        Manager = manager;
+    }
+
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string? Description { get; private set; }
+    
+    public Person Manager { get; private set; }
+
+    private List<Person> _organizers = new();
+    public IReadOnlyCollection<Person> Organizers => _organizers;
+
+    private List<Person> _motivators = new();
+    public IReadOnlyCollection<Person> Motivators => _motivators;
+
+    private List<Person> _contacts = new();
+    public IReadOnlyCollection<Person> Contacts => _contacts;
+
+    public static Campaign Create(string title, Person manager, string? description = null)
+    {
+        return new(title, description, manager);
+    }
+    
+    public Person AddOrganizer(string shortName, string firstName, string lastName, string phoneNumber, string email)
+    {
+        var organizer = Person.CreateOrganizer(shortName, firstName, lastName, phoneNumber, email);
+        
+        _organizers.Add(organizer);
+
+        return organizer;
+    }
+
+    public Person AddMotivator(string shortName, string phoneNumber,
+        string? email = null, string? firstName = null, string? lastName = null)
+    {
+        var motivator = Person.CreateMotivator(shortName, firstName ?? string.Empty, lastName ?? string.Empty, phoneNumber, email);
+        
+        _motivators.Add(motivator);
+
+        return motivator;
+    }
+}
---
+++ CaMan.Domain/Person.cs
@@ -0,0 +1,68 @@
+﻿namespace CaMan.Domain;
+
+public sealed class Person
+{
+    private Person(string shortName, string firstName, string lastName, PersonRole role, PersonAgeGroup ageGroup, string? email, string? phoneNumber)
+    {
+        Id = Guid.NewGuid();
+        
+        ShortName = shortName;
+        FirstName = firstName;
+        LastName = lastName;
+        Role = role;
+        AgeGroup = ageGroup;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public Guid Id { get; private set; }
+    public string ShortName { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string? Email { get; private set; }
+    public string? PhoneNumber { get; private set; }
+    public PersonRole Role { get; private set; }
+    public PersonAgeGroup AgeGroup { get; private set; }
+    
+    internal static Person CreateContact(string shortName, string firstName, string lastName, string? phoneNumber = null, string? email = null, PersonAgeGroup ageGroup = PersonAgeGroup.Uknown)
+    {
+        if (string.IsNullOrWhiteSpace(phoneNumber) && string.IsNullOrWhiteSpace(email))
+        {
+            throw new Exception("phone or email is required for a contact");
+        }
+        
+        return new(shortName, firstName, lastName, PersonRole.Contact, ageGroup, email, phoneNumber);
+    }
+    
+    internal static Person CreateMotivator(string shortName, string firstName, string lastName, string phoneNumber, string? email = null, PersonAgeGroup ageGroup = PersonAgeGroup.Uknown)
+    {
+        return new(shortName, firstName, lastName, PersonRole.Motivator, ageGroup, email, phoneNumber);
+    }
+    
+    internal static Person CreateOrganizer(string shortName, string firstName, string lastName, string phoneNumber, string email, PersonAgeGroup ageGroup = PersonAgeGroup.Uknown)
+    {
+        return new(shortName, firstName, lastName, PersonRole.Organizer, ageGroup, email, phoneNumber);
+    }
+    
+    internal static Person CreateManager(string shortName, string firstName, string lastName, string phoneNumber, string email, PersonAgeGroup ageGroup = PersonAgeGroup.Uknown)
+    {
+        return new(shortName, firstName, lastName, PersonRole.Manager, ageGroup, email, phoneNumber);
+    }
+    
+    public enum PersonAgeGroup
+    {
+        Uknown,
+        Student,
+        Youth,
+        MiddleAged,
+        Elder
+    }
+
+    public enum PersonRole
+    {
+        Contact,
+        Motivator,
+        Organizer,
+        Manager
+    }
+}
