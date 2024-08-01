3045132915564c887ca33bec3b3064436aca5829
v.paschalidis-cc <v.paschalidis-cc@kaizengaming.com>
2024-08-01 12:57:42 +0300

    add efcore configuration project

---
+++ CaMan.Domain.EfConfiguration/CampaignConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CaMan.Domain.Campaigns;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CaMan.Domain.EfConfiguration;
+
+internal class CampaignConfiguration : IEntityTypeConfiguration<Campaign>
+{
+    public void Configure(EntityTypeBuilder<Campaign> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasConversion(p => p.Value.ToString(), value => new CampaignId(Ulid.Parse(value)));
+    }
+}
